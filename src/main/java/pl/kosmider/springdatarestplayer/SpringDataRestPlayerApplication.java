8b09b079483bcbe116e6ffa74406189fb422dd62
KosmiderPrzemyslaw <kosmiderprzemyslaw91@gmail.com>
2024-01-09 20:55:01 +0100

    first commit

---
+++ src/main/java/pl/kosmider/springdatarestplayer/SpringDataRestPlayerApplication.java
@@ -0,0 +1,13 @@
+package pl.kosmider.springdatarestplayer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataRestPlayerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataRestPlayerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/kosmider/springdatarestplayer/SpringDataRestPlayerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kosmider.springdatarestplayer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataRestPlayerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
