131091770a297d83d5c5593061d995afde656004
KosmiderPrzemyslaw <kosmiderprzemyslaw91@gmail.com>
2024-01-11 06:21:12 +0100

    add crud coach implementation

--- src/main/java/pl/kosmider/springdatarestplayer/controller/CoachController.java
+++ src/main/java/pl/kosmider/springdatarestplayer/controller/CoachController.java
@@ -1,12 +1,12 @@
 package pl.kosmider.springdatarestplayer.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pl.kosmider.springdatarestplayer.entity.Coach;
 import pl.kosmider.springdatarestplayer.repository.CoachRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class CoachController {
@@ -19,7 +19,45 @@ public class CoachController {
     }
 
     @GetMapping("/coaches")
-    public List<Coach> coaches(){
+    public List<Coach> coaches() {
         return coachRepository.findAll();
+    }
+
+    @GetMapping("coach/{coachId}")
+    public Coach findCoachById(@RequestParam Long coachId) {
+        Optional<Coach> coachById = coachRepository.findById(coachId);
+        Coach coach = null;
+
+        if (coachById.isPresent()) {
+            coach = coachById.get();
+        } else {
+            throw new RuntimeException("Can not find coach with id " + coachId);
+        }
+        return coach;
+    }
+
+    @PostMapping("/coach")
+    public Coach saveCoach(@RequestBody Coach coach) {
+        coach.setId(0L);
+        return coachRepository.save(coach);
+    }
+
+    @PutMapping
+    public Coach updateCoach(@RequestBody Coach coach) {
+        return coachRepository.save(coach);
+    }
+
+    @DeleteMapping
+    public Coach deleteCoach(@RequestParam Long coachId) {
+        Optional<Coach> coach = coachRepository.findById(coachId);
+
+        Coach coachFromDb = null;
+
+        if (coach.isPresent()) {
+            coachRepository.delete(coachFromDb);
+        } else {
+            throw new RuntimeException("Can not find coach with id -" + coachId);
+        }
+        return coachFromDb;
     }
 }
